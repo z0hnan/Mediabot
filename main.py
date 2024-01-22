186d6d0dec9d1d12ac44be3c87ca8954df0ecab7
z0hnan <simesim2006@gmail.com>
2024-01-22 20:56:26 +0100

    let the cooking begin

--- main.py
+++ main.py
@@ -1,30 +1,52 @@
 import discord
-import asyncio
+from discord.ext import commands
+import youtube_dl
 
-#Discord bot settings
-intents = discord.Intents.all()
-client = discord.Client(command_prefix='!', intents=intents,activity=discord.Game(name='Escape from Tarkov'))
+intents = discord.Intents.default()
+intents.all()
+
+# Create a bot instance with a command prefix and intents
+bot = commands.Bot(command_prefix='!', intents=intents)
 
 #Declaring variables
 tracked_servers = set()
 
+async def join_channel(ctx):
+    channel = ctx.author.voice.channel
+    return await channel.connect()
+
 
 #Discord bot startup
-@client.event
+@bot.event
 async def on_ready():
-    pingCounter = 0
-    print('Logged in as {0.user}'.format(client))
+    print('Logged in as {0.user}'.format(bot))
     print('Connected to servers:')
-    for guild in client.guilds:
+    for guild in bot.guilds:
         tracked_servers.add(guild.id)
         print(guild.name)
 
 
- #Bot recieves message
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
+@bot.command(name='play')
+async def play(ctx, url):
+    voice_channel = ctx.guild.voice_client
+    if not voice_channel.is_connected():     
+        voice_channel = await join_channel(ctx)
+
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        url2 = info['formats'][0]['url']
+
+    voice_channel.play(discord.FFmpegPCMAudio(url2), after=lambda e: print('done', e))
 
 
-client.run('--- YOUR TOKEN HERE ---')
+
+bot.run('heheheheheh')
